fb1c641029ce73c5f2c1be1a4f16c65c6de074c4
Abdelkarim Essalim <karim.essalim@gmail.com>
2021-01-03 01:44:28 +0200

    1.9 unicafe step4 suoritettu

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,26 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
+  if (good + bad + neutral === 0) {
+    return <div>No feedback given</div>;
+  }
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>good {good} </p>
+      <p>neutral {neutral} </p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>
+        average {(good * 1 + neutral * 0 + bad * -1) / (good + bad + neutral)}
+      </p>
+      <p>positive {(good / (good + neutral + bad)) * 100}%</p>
+    </div>
+  );
+};
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
@@ -13,10 +33,9 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>Statistics</h1>
-      <p>good {good} </p>
-      <p>neutral {neutral} </p>
-      <p>bad {bad}</p>
+      <br />
+      <br />
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
