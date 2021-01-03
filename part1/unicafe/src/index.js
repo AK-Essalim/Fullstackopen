1ecf14b89f8a96fb5027cc9c4a21cfe49d664449
Abdelkarim Essalim <karim.essalim@gmail.com>
2021-01-03 02:31:26 +0200

    1.11 unicafe step6 completed

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,33 +6,75 @@ const Statistics = (props) => {
   if (good + bad + neutral === 0) {
     return <div>No feedback given</div>;
   }
+
   return (
     <div>
       <h1>Statistics</h1>
-      <p>good {good} </p>
-      <p>neutral {neutral} </p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>
-        average {(good * 1 + neutral * 0 + bad * -1) / (good + bad + neutral)}
-      </p>
-      <p>positive {(good / (good + neutral + bad)) * 100}%</p>
+      <table>
+        <tbody>
+          <tr>
+            <StatisticLine text="good" value={good} />
+          </tr>
+          <tr>
+            <StatisticLine text="neutral" value={neutral} />
+          </tr>
+          <tr>
+            <StatisticLine text="bad" value={bad} />
+          </tr>
+          <tr>
+            <StatisticLine text="all" value={good + neutral + bad} />
+          </tr>
+          <tr>
+            <StatisticLine
+              text="average"
+              value={
+                (good * 1 + neutral * 0 + bad * -1) / (good + bad + neutral)
+              }
+            />
+          </tr>
+          <tr>
+            <StatisticLine
+              text="positive"
+              value={(good / (good + neutral + bad)) * 100}
+              end="%"
+            />
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
 
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+const StatisticLine = (props) => {
+  return (
+    <>
+      <td>{props.text}</td>
+      <td>
+        {props.value} {props.end}
+      </td>
+    </>
+  );
+};
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const increaseGood = () => setGood(good + 1);
+  const increaseNeutral = () => setNeutral(neutral + 1);
+  const increaseBad = () => setBad(bad + 1);
+
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button text="good" handleClick={() => increaseGood()} />
+      <Button text="neutral" handleClick={() => increaseNeutral()} />
+      <Button text="bad" handleClick={() => increaseBad()} />
+
       <br />
       <br />
       <Statistics good={good} bad={bad} neutral={neutral} />
