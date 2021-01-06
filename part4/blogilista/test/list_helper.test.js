49c002751c7304a7c2b343a4cc2fec384a210b39
Abdelkarim Essalim <karim.essalim@gmail.com>
2021-01-06 22:57:47 +0200

    favoriteBlog working with my own solution, not lodash

--- part4/blogilista/test/list_helper.test.js
+++ part4/blogilista/test/list_helper.test.js
@@ -55,6 +55,18 @@ const blogs = [
   },
 ];
 
+const array1 = [
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url:
+      "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+];
+
 test("dummy returns one", () => {
   const blogit = [];
 
@@ -71,20 +83,9 @@ describe("total likes", () => {
   });
 
   test("when list has only one blog equal the likes of that", () => {
-    const arr = [
-      {
-        _id: "5a422aa71b54a676234d17f8",
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url:
-          "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5,
-        __v: 0,
-      },
-    ];
-    const result = listHelper.totalLikes(arr);
+    const result = listHelper.totalLikes(array1);
 
-    expect(result).toBe(arr[0].likes);
+    expect(result).toBe(array1[0].likes);
   });
   test("of a bigger list is calculated correctly", () => {
     const arr = [];
@@ -96,17 +97,21 @@ describe("total likes", () => {
 
 describe("favorite blog", () => {
   test("when array is empty returns null", () => {
-    favoriteBlog([{}]).toBe(0);
+    expect(favoriteBlog([{}])).toBeNull();
   });
 
   test("when testing with all blogs is correct", () => {
-    expect(listHelper.favoriteBlog([{}])).toEqual({
-      _id: "5a422b3a1b54a676234d17f9",
+    expect(favoriteBlog(blogs)).toEqual({
       title: "Canonical string reduction",
       author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
       likes: 12,
-      __v: 0,
+    });
+  });
+  test("when using 1 blog is correct", () => {
+    expect(favoriteBlog(array1)).toEqual({
+      title: array1[0].title,
+      author: array1[0].author,
+      likes: array1[0].likes,
     });
   });
 });
--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -1,5 +1,5 @@
 const blog = require("../models/blog");
-const _ = require("lodash")
+const _ = require("lodash");
 
 const dummy = (array) => {
   return 1;
@@ -12,25 +12,41 @@ const totalLikes = (arrayReceived) => {
 };
 
 const favoriteBlog = (blogs) => {
-    const a = _.sortBy(blogs, ["likes"])
+  const a = _.sortBy(blogs, ["likes"]);
+  if (!blogs.length) {
+    return null;
+  }
+  if (blogs[0].likes === undefined) {
+    return null;
+  }
+  if (blogs.length === 1)
+    return {
+      title: blogs[0].title,
+      author: blogs[0].author,
+      likes: blogs[0].likes,
+    };
 
-    return a[blogs.length -1]
+  let max = 0;
+  let index = 0;
+  let favorite = [];
+  let i = 0;
 
-}
+  for (i; i < blogs.length; i++) {
+    if (blogs[i].likes > max) {
+      max = blogs[i].likes;
+      index = i;
+    }
+  }
 
+  const theOne = {
+    title: blogs[index].title,
+    author: blogs[index].author,
+    likes: blogs[index].likes,
+  };
+  return theOne;
 
-  const reducer = blogs.reduce(
-    (fav, cur) => (cur.likes >= fav.likes ? cur : fav),
-    { likes: 0 }
-  );
-  console.log(reducer);
-  return reducer.length === 1 ? blogs[0] : reducer;
-};
-const mostBlogs = (blogs) => {
-  return blogs.length === 0 ? 0 : 0;
-};
-const mostLikes = (blogs) => {
-  return blogs.length === 0 ? 0 : 0;
+  //Works by lodash but unwanted
+  //return _.maxBy(blogs, "likes");
 };
 
 module.exports = {
